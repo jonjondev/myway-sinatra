38331f0ab69ec3cf21a74cde70485b3c31bdac1a
Jonathan Moallem <jdmoallem@gmail.com>
2018-04-30 16:55:59 +1000

    Added variable imports for database adapter

--- config/models.rb
+++ config/models.rb
@@ -2,22 +2,29 @@
 require 'data_mapper'
 require 'dm-core'
 require 'dm-timestamps'
-require 'dm-postgres-adapter'
-require 'dm-sqlite-adapter'
 
 # Require the models
 require File.expand_path(File.join('models', 'base_model_properties'))
 Dir[File.join('models', '**/*_model.rb')].each { |file| require File.expand_path(file) }
 
+def try_import name
+	begin
+	  require name 
+	rescue Error
+	  print "Import Error: #{name} not found, add to Gemfile."
+	end
+end
 
 # Configure database environment values for testing
 configure :test do
+	try_import('dm-sqlite-adapter')
   DataMapper.setup(:default, 'sqlite::memory:')
 end
 
 # :nocov:
 # Configure database environment values for development
 configure :development do
+	try_import('dm-postgres-adapter')
   DataMapper.setup(:default, 'postgres://myway:myway@localhost:5432/myway')
 end
 
